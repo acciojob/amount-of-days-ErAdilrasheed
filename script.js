02cf6e5566ea04ce43377787134b19d076ec4cbb
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2025-04-30 01:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if (year % 4 == 0 && year % 100 != 0) {
+		return console.log("366")
+	} else if(year % 400 == 0){
+		return console.log("366")
+	} else{
+		return console.log("365")
+	}
+}
+daysOfAYear();
